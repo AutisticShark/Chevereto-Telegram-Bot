0888d1c1f9d4414f88cd15194f6450800186136e
SuicidalCat <14369594+SuicidalCat@users.noreply.github.com>
2018-12-19 20:35:44 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from functools import wraps
 import os
-import os.path 
+import os.path
+import sys
+from threading import Thread 
 import glob
 import telegram
 import requests
@@ -21,95 +23,6 @@ logger = logging.getLogger(__name__)
 #加載config
 config = configparser.ConfigParser()
 config.read('config.ini')
-
-def send_typing_action(function):
-    @wraps(function)
-    def command_function(*args, **kwargs):
-        bot, update = args
-        bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
-        function(bot, update, **kwargs)
-    return command_function
-
-@send_typing_action
-def help(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Send me some pictures or image file. Available format: .jpg, .png, .bmp, .gif, 20MB max file size.')
-
-def privacy(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="This bot is only designed to rely your media file, all of your presonal data will not storage on our server, for more usage info please check the image host's ToS/AUP site, thank you.")
-
-def uptime(bot, update):
-    uptime_command = os.popen("uptime")
-    uptime_output = uptime_command.read()
-    bot.send_message(chat_id=update.message.chat_id, text=uptime_output)
-
-def storage_status(bot, update):
-    storage_status_command = os.popen("df -lh")
-    storage_status_output = storage_status_command.read()
-    bot.send_message(chat_id=update.message.chat_id, text=storage_status_output)
-
-def cache_status(bot, update):
-    cache_path = os.getcwd()
-    cache_files_count = str(len([name for name in os.listdir(cache_path) if os.path.isfile(os.path.join(cache_path, name))]) - 1)
-    cache_files_size = str(cache_files_size_count(cache_path))
-    cache_status_message = 'Current cache status:\nCache files count: ' + cache_files_count + '\nCache files size: ' + cache_files_size
-    bot.send_message(chat_id = update.message.chat_id, text = cache_status_message)
-
-def cache_files_size_count(cache_path):
-    size = 0
-    for dirpath, dirnames, filenames in os.walk(cache_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            size += os.path.getsize(fp)
-    return size
-
-def cache_clean(bot, update):
-    cache_path = os.getcwd()
-    cache_files_list = glob.glob(os.path.join(cache_path, "*.jpg"))
-    for cache in cache_files_list:
-        os.remove(cache) 
-    bot.send_message(chat_id=update.message.chat_id, text='All upload cache are cleared')
-
-def restart_action():
-    updater.stop()
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
-def restart(bot, update):
-    update.message.reply_text('Bot is restarting...')
-    Thread(target=stop_and_restart).start()
-
-@send_typing_action
-def unknow_msg(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Please send me pictures or image file only!')
-
-@send_typing_action
-def image(bot, update):
-    image_id = update.message.photo[-1].file_id
-    image_name = '%s.jpg' % str(uuid.uuid4())
-    image = bot.getFile(image_id)
-    image.download(image_name)
-    update.message.reply_text('Download complete, now uploading...')
-    return_data = image_upload(request_format(image_name))
-    if return_data['status_code'] == 200:
-        uploaded_info = 'Upload succeeded!\nHere are your links to this image:\nWeb viewer: ' + return_data['image']['url_viewer'] + '\nOrigin size: ' + return_data['image']['url']# + '\n Medium size:' + return_data['medium']['url']
-        update.message.reply_text(uploaded_info)
-    else:
-        update.message.reply_text('Image Host error! Please try again later.')
-
-def image_upload(images):
-    image_host = config['HOST']['IMAGE_HOST']
-    image_host_api_key = config['HOST']['IMAGE_HOST_API_KEY']
-    image_host_return_format = config['HOST']['IMAGE_HOST_RETURN_FORMAT']
-    request_url = 'https://' + image_host + '/api/1/upload/?key=' + image_host_api_key + '&format=' + image_host_return_format
-    upload_response = requests.post(request_url, files = images)
-    print(upload_response)
-    return upload_response.json()
-#構造upload請求
-def request_format(image_name):
-    image_upload_request = []
-    image_type = mimetypes.guess_type(image_name)[0]
-    image_upload_request.append(('source' , (image_name , open(image_name , 'rb') , image_type)))
-    print(image_upload_request)
-    return image_upload_request
 
 def main():
     updater = Updater(config['BOT']['ACCESS_TOKEN'])#填你bot的API Key
@@ -134,6 +47,95 @@ def main():
     #處理用戶私聊發送的未知訊息
     unknow_msg_handler = MessageHandler(Filters.private, unknow_msg)
     dp.add_handler(unknow_msg_handler)
+    #handlers
+    def send_typing_action(function):
+        @wraps(function)
+        def command_function(*args, **kwargs):
+            bot, update = args
+            bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+            function(bot, update, **kwargs)
+        return command_function
+
+    @send_typing_action
+    def help(bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text='Send me some pictures or image file. Available format: .jpg, .png, .bmp, .gif, 20MB max file size.')
+
+    def privacy(bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text="This bot is only designed to rely your media file, all of your presonal data will not storage on our server, for more usage info please check the image host's ToS/AUP site, thank you.")
+
+    def uptime(bot, update):
+        uptime_command = os.popen("uptime")
+        uptime_output = uptime_command.read()
+        bot.send_message(chat_id=update.message.chat_id, text=uptime_output)
+
+    def storage_status(bot, update):
+        storage_status_command = os.popen("df -lh")
+        storage_status_output = storage_status_command.read()
+        bot.send_message(chat_id=update.message.chat_id, text=storage_status_output)
+
+    def cache_status(bot, update):
+        cache_path = os.getcwd()
+        cache_files_count = str(len([name for name in os.listdir(cache_path) if os.path.isfile(os.path.join(cache_path, name))]) - 1)
+        cache_files_size = str(cache_files_size_count(cache_path))
+        cache_status_message = 'Current cache status:\nCache files count: ' + cache_files_count + '\nCache files size: ' + cache_files_size
+        bot.send_message(chat_id = update.message.chat_id, text = cache_status_message)
+
+    def cache_files_size_count(cache_path):
+        size = 0
+        for dirpath, dirnames, filenames in os.walk(cache_path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                size += os.path.getsize(fp)
+        return size
+
+    def cache_clean(bot, update):
+        cache_path = os.getcwd()
+        cache_files_list = glob.glob(os.path.join(cache_path, "*.jpg"))
+        for cache in cache_files_list:
+            os.remove(cache) 
+        bot.send_message(chat_id=update.message.chat_id, text='All upload cache are cleared')
+
+    def restart_action():
+        updater.stop()
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
+    def restart(bot, update):
+        update.message.reply_text('Bot is restarting...')
+        Thread(target=restart_action).start()
+
+    @send_typing_action
+    def unknow_msg(bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text='Please send me pictures or image file only!')
+
+    @send_typing_action
+    def image(bot, update):
+        image_id = update.message.photo[-1].file_id
+        image_name = '%s.jpg' % str(uuid.uuid4())
+        image = bot.getFile(image_id)
+        image.download(image_name)
+        update.message.reply_text('Download complete, now uploading...')
+        return_data = image_upload(request_format(image_name))
+        if return_data['status_code'] == 200:
+            uploaded_info = 'Upload succeeded!\nHere are your links to this image:\nWeb viewer: ' + return_data['image']['url_viewer'] + '\nOrigin size: ' + return_data['image']['url']# + '\n Medium size:' + return_data['medium']['url']
+            update.message.reply_text(uploaded_info)
+        else:
+            update.message.reply_text('Image Host error! Please try again later.')
+
+    def image_upload(images):
+        image_host = config['HOST']['IMAGE_HOST']
+        image_host_api_key = config['HOST']['IMAGE_HOST_API_KEY']
+        image_host_return_format = config['HOST']['IMAGE_HOST_RETURN_FORMAT']
+        request_url = 'https://' + image_host + '/api/1/upload/?key=' + image_host_api_key + '&format=' + image_host_return_format
+        upload_response = requests.post(request_url, files = images)
+        print(upload_response)
+        return upload_response.json()
+    #構造upload請求
+    def request_format(image_name):
+        image_upload_request = []
+        image_type = mimetypes.guess_type(image_name)[0]
+        image_upload_request.append(('source' , (image_name , open(image_name , 'rb') , image_type)))
+        print(image_upload_request)
+        return image_upload_request
     #啓動進程
     updater.start_polling()
     updater.idle()
