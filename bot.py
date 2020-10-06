311b9dd8fc4dea542f511b05d02b995ed989b74d
兩足大貓貓 <yihan.huang@manage.hkserversolution.com>
2020-10-06 08:53:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,7 +91,7 @@ def main():
     def image(update, context):
         image_id = update.message.photo[-1].file_id
         image_name = '%s.jpg' % str(uuid.uuid4())
-        image = bot.getFile(image_id)
+        image = context.bot.getFile(image_id)
         image.download(image_name)
         update.message.reply_text('Download complete, now uploading to image host...')
         return_data = image_upload(request_format(image_name))
@@ -108,7 +108,7 @@ def main():
         allowed_image_file_format = 'image/jpeg image/png image/bmp image/gif'
         image_file_id = update.message.document.file_id
         image_file_name = '%s.cache' % str(uuid.uuid4())
-        image_file = bot.getFile(image_file_id)
+        image_file = context.bot.getFile(image_file_id)
         image_file.download(image_file_name)
         image_file_mime = magic.from_file(image_file_name, mime=True)
         if image_file_mime in allowed_image_file_format:
