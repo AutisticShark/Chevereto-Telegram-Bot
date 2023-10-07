df4123ec07bc0669f7e60ca3ebace9e929a0a8e5
M1Screw <14369594+M1Screw@users.noreply.github.com>
2023-10-07 10:46:13 +0800

    refactor: separate image upload def

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import configparser
 import logging
 import os
 import os.path
-import shutil
 import uuid
 from functools import wraps
 
@@ -14,13 +13,13 @@ import requests
 import telegram
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
-# 錯誤logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING)
-logger = logging.getLogger(__name__)
-
 # 加載config
 config = configparser.ConfigParser()
 config.read('config.ini')
+# 錯誤logging
+logging_level = 'logging.' + config['BOT']['LOGGING_LEVEL']
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging_level)
+logger = logging.getLogger(__name__)
 
 
 # handler functions
@@ -79,9 +78,7 @@ async def cache_clean(update, context):
     cache_path = os.getcwd() + '/cache'
     cache_files_list = os.listdir(cache_path)
     for cache in cache_files_list:
-        if cache.endswith(".jpg"):
-            os.remove(os.path.join(cache_path, cache))
-        elif cache.endswith(".cache"):
+        if cache.endswith(".jpg") or cache.endswith(".cache"):
             os.remove(os.path.join(cache_path, cache))
     await context.bot.send_message(chat_id=update.message.chat_id, text='All upload cache are cleared.')
 
@@ -95,22 +92,11 @@ async def unknown_msg(update, context):
 async def image(update, context):
     image_id = update.message.photo[-1].file_id
     image_name = '%s.jpg' % str(uuid.uuid4())
-    image = await context.bot.get_file(image_id)
-    await image.download_to_drive(image_name)
+    image_path = os.getcwd() + '/cache/' + image_name
+    image_raw = await context.bot.get_file(image_id)
+    await image_raw.download_to_drive(image_path, image_name)
     reply_message = await update.message.reply_text('Downloading image from Telegram server...')
-    return_data = image_upload(request_format(image_name))
-    if return_data['status_code'] == 200:
-        shutil.move(image_name, 'cache/' + image_name)
-        url_viewer = return_data['image']['url_viewer']
-        url = return_data['image']['url']
-        uploaded_info = f'Upload succeeded!\n' \
-                        f'Web viewer: {url_viewer}\n' \
-                        f'Origin size: {url}'
-        await reply_message.edit_text(uploaded_info)
-    else:
-        print(return_data)
-        await reply_message.edit_text('Image Host error! Please try again later.')
-        os.remove(image_name)
+    await reply_message.edit_text(image_upload(image_path))
 
 
 @send_typing_action
@@ -118,37 +104,41 @@ async def image_file(update, context):
     allowed_image_file_format = config['HOST']['ALLOWED_FILE_MINE']
     image_file_id = update.message.document.file_id
     image_file_name = '%s.cache' % str(uuid.uuid4())
-    image_file = await context.bot.get_file(image_file_id)
-    await image_file.download_to_drive(image_file_name)
-    image_file_mime = magic.from_file(image_file_name, mime=True)
+    image_file_path = os.getcwd() + '/cache/' + image_file_name
+    image_file_raw = await context.bot.get_file(image_file_id)
+    await image_file_raw.download_to_drive(image_file_path, image_file_name)
+    image_file_mime = magic.from_file(image_file_path, mime=True)
     if image_file_mime in allowed_image_file_format:
         reply_message = await update.message.reply_text('Downloading image file from Telegram server...')
-        return_data = image_upload(request_format(image_file_name))
-        if return_data['status_code'] == 200:
-            shutil.move(image_file_name, 'cache/' + image_file_name)
-            url_viewer = return_data['image']['url_viewer']
-            url = return_data['image']['url']
-            uploaded_info = f'Upload succeeded!\n' \
-                            f'Web viewer: {url_viewer}\n' \
-                            f'Origin size: {url}'
-            await reply_message.edit_text(uploaded_info)
-        else:
-            print(return_data)
-            await reply_message.edit_text('Image Host error! Please try again later.')
-            os.remove(image_file_name)
+        await reply_message.edit_text(image_upload(image_file_path))
     else:
         allowed_format = config['HOST']['ALLOWED_FILE_FORMAT']
         await update.message.reply_text(f'Please send me {allowed_format} format image file only!')
+        os.remove(image_file_path)
+
+
+def image_upload(image_file_name):
+    return_data = do_image_upload(request_format(image_file_name))
+    if return_data['status_code'] == 200:
+        url_viewer = return_data['image']['url_viewer']
+        url = return_data['image']['url']
+        uploaded_info = f'Upload succeeded!\n' \
+                        f'Web viewer: {url_viewer}\n' \
+                        f'Origin size: {url}'
+        return uploaded_info
+    else:
+        logger.error(return_data)
         os.remove(image_file_name)
+        return 'Image Host error! Please try again later.'
 
 
-def image_upload(images):
+def do_image_upload(images):
     image_host = config['HOST']['IMAGE_HOST']
     image_host_api_key = config['HOST']['IMAGE_HOST_API_KEY']
     image_host_return_format = config['HOST']['IMAGE_HOST_RETURN_FORMAT']
     request_url = f'https://{image_host}/api/1/upload/?key={image_host_api_key}&format={image_host_return_format}'
     upload_response = requests.post(request_url, files=images)
-    print(upload_response)
+    logger.info(upload_response)
     return upload_response.json()
 
 
@@ -157,13 +147,13 @@ def request_format(image_name):
     image_upload_request = []
     image_type = magic.from_file(image_name, mime=True)
     image_upload_request.append(('source', (image_name, open(image_name, 'rb'), image_type)))
-    print(image_type)
-    print(image_upload_request)
+    logger.info(image_type + str(image_upload_request))
     return image_upload_request
 
 
 def main():
     bot = Application.builder().token(config['BOT']['ACCESS_TOKEN']).build()
+    admin_user_id = int(config['BOT']['ADMIN_USER_ID'])
     # handlers
     # /help指令處理
     bot.add_handler(CommandHandler("help", help))
@@ -171,13 +161,13 @@ def main():
     bot.add_handler(CommandHandler("uptime", uptime))
     # /storage_status指令處理
     bot.add_handler(
-        CommandHandler("storage_status", storage_status, filters=filters.User(user_id=config['BOT']['ADMIN_USER_ID'])))
+        CommandHandler("storage_status", storage_status, filters=filters.User(admin_user_id)))
     # /cache_status指令處理
     bot.add_handler(
-        CommandHandler("cache_status", cache_status, filters=filters.User(user_id=config['BOT']['ADMIN_USER_ID'])))
+        CommandHandler("cache_status", cache_status, filters=filters.User(admin_user_id)))
     # /cache_clean指令處理
     bot.add_handler(
-        CommandHandler("cache_clean", cache_clean, filters=filters.User(user_id=config['BOT']['ADMIN_USER_ID'])))
+        CommandHandler("cache_clean", cache_clean, filters=filters.User(admin_user_id)))
     # 處理用戶發送的圖片
     image_handler = MessageHandler(filters.PHOTO, image)
     bot.add_handler(image_handler)
